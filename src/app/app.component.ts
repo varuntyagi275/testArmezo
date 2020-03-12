019f0e23b79128d584f002506c5e6535f4405bdc
Varun Tyagi <varuntyagi275@gmail.com>
2020-03-12 10:53:38 +0530

    teset Armezo
    Test Armezo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray, transferArrayItem, CdkDrag, CdkDragExit, CdkDragEnter } from '@angular/cdk/drag-drop';
+import {ViewChild, ElementRef} from '@angular/core';
+import { read } from 'fs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testArmezo';
-}
+  @ViewChild('doneList',{static: true}) doneList:ElementRef;
+  todo = [];
+  done = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  review = [];
+  showPlaceholder: "add Item Here";
+
+countOfDropContainer:number=0;
+   drop(event: CdkDragDrop<string[]>) {
+    if(event.container.id==="donelist"){
+      console.log("this is home");
+    }else if( event.container.data.length>4){
+      alert("conatainer is full");
+      return;
+    }
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+         
+      transferArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+                       
+    }
+  
+      }
+
+      entered(event: CdkDragEnter<string[]>) {
+        console.log('Entered', event.item.data);
+       }
+      exited(event: CdkDragExit<string[]>){
+
+        console.log('Exited', event.item.data);
+      }
+       /** Predicate function that only allows even numbers to be dropped into a list. */
+ 
+
+  }
+
+ 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ChipsComponent } from './chips/chips.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChipsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    DragDropModule
   ],
   providers: [],
   bootstrap: [AppComponent]
