9f8d7b30c743c4ea4572da863289f5c54fb9b547
Varun Tyagi <varuntyagi275@gmail.com>
2020-03-12 16:25:35 +0530

    rebinding is not complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,11 @@ import { ViewChild, ElementRef } from '@angular/core';
 export class AppComponent {
   @ViewChild('doneList', { static: true }) doneList: ElementRef;
   todo = [];
-  done = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  done=[{value:1,disabled:false},{value:2,disabled:false},{value:3,disabled:false},{value:4,disabled:false},{value:5,disabled:false},{value:6,disabled:false},{value:7,disabled:false},{value:8,disabled:false},{value:9,disabled:false},{value:10,disabled:false},{value:11,disabled:false}]
+  //done = [1, 2, 3,4, 5, 6, 7, 8, 9, 10, 11, ];
   review = [];
 
-  countOfDropContainer: number = 0;
+  
   drop(event: CdkDragDrop<string[]>) {
     if (event.container.id === "donelist") {
       console.log("this is home");
@@ -25,7 +26,9 @@ export class AppComponent {
     if (event.previousContainer === event.container) {
       moveItemInArray(this.done, event.previousIndex, event.currentIndex);
     } else {
-      if (event.previousContainer.id === "donelist" && event.previousContainer !== event.container) {
+      if (event.previousContainer.id === "donelist"
+      ) 
+      {
         copyArrayItem(
           event.previousContainer.data,
           event.container.data,
@@ -34,30 +37,33 @@ export class AppComponent {
         );
 
         if (event.previousContainer.exited) {
-          event.item.disabled = true;
+          event.item.disabled=true
+          
+         } 
+        }else {
 
-        }
-      else {
+                 if (event.previousContainer === event.container) 
+                 {
+                  console.log("container is same. dont disable the item")
+                 }
+                 transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+                 
+                   function onlyUnique(value, index, self) {
+                   return self.indexOf(value) === index;
+                    }
+                    var unique = this.done.filter(onlyUnique);
+                    this.done = unique
+                     console.log(this.done)
+                      event.container.data.splice(1,1,event.item.toString())
+                      if(event.item.disabled===true){
+                       event.item.disabled=false;
+                       console.log(event.item.disabled+ "will Do that if asked"); 
+                      }
+               
+                }
 
-        if (event.previousContainer === event.container) {
-          console.log("container is same. dont disable the item")
-        }
-        transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
-        // this.done.splice(1,1,event.item.data);
-        function onlyUnique(value, index, self) {
-          return self.indexOf(value) === index;
-        }
-        var unique = this.done.filter(onlyUnique);
-        this.done = unique
-        console.log(this.done)
-        event.container.
-                         
-    }
 
     }
   }
 
 }
-
-}
-
